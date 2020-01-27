b5d041e306bf9a169af371a6e5303ced3da2e04c
howtotailslide <agasbarro36@hotmail.com>
2020-01-27 10:16:26 -1000

    Initial commit

---
+++ TLCalculations.m
@@ -0,0 +1,10 @@
+%Computation for Transmission Line Geometry #3
+%By: Anthony Gasbarro
+heightMesh = 2.02; 
+widthMesh = 7.0;  
+heightDielectric = 1.00;
+voltageConductor = 10.00 
+widthConductor = 1.00;
+t = 0.01;
+c1 = c0;
+c2 = 9.6*c0;
---
+++ assign1.m
@@ -0,0 +1,5 @@
+Amatrix = zeros(21, 21)
+for i = 1:21
+    Amatrix(i,i) = -4
+end
+for i:
